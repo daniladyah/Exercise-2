c24ba5bb71b673536ff66341d6376222f5f9f381
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-31 16:25:18 +0700

    Add project files.

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        //definidikan array dengan ukuran maksimum 40
+        private int[] Danila = new int[40];
+        static void Main(string[] args)
+        {
+        }
+    }
+}
