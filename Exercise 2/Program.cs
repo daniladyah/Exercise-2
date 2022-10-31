e17da7c05afa66704b6b10c2e837802a5e404453
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-10-31 16:37:16 +0700

    menambahkan jumlah element/data array

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -14,7 +14,20 @@ namespace Exercise_2
         private int n;
         //fungsi untuk menginputkan ukuran array
         public void read()
-        { }
+        {
+            while (true)
+            {
+                Console.Write("masukkan jumlah element/data array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 40)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 40 elemen.\n");
+
+
+            }
+        }
         static void Main(string[] args)
         {
         }
